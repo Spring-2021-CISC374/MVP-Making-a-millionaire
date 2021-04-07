fde4479c8b6d53573a3ecdba0e831b39c719cb30
Christoffer Braun <braunc@udel.edu>
2021-04-07 17:01:54 -0400

    Scene1 button sounds

--- Scene1.js
+++ Scene1.js
@@ -10,6 +10,7 @@ class Scene1 extends Phaser.Scene {
         this.load.image("menu_btn","assets/images/menu_btn.png");
         this.load.image("shop_btn","assets/images/shop_btn.png");
         this.load.image("settings_btn","assets/images/settings_btn.png");
+        this.load.audio("click","sounds/goodclick.mp3");
     }
 
     create() {
@@ -20,23 +21,28 @@ class Scene1 extends Phaser.Scene {
 
 
         let config = this.game.config;
+
+        this.clickSound = this.sound.add("click");
         
         this.startBtn = this.add.image("400", "225", "start_btn")
         .setInteractive()
         .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
             this.input.on('pointerdown', () => this.scene.start('playGame'))
+            this.clickSound.play();
         })
 
         this.shopBtn = this.add.image("400", "300", "shop_btn")
         .setInteractive()
         .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
             this.input.on('pointerdown', () => this.scene.start('shopScene'))
+            this.clickSound.play();
         })
 
         this.settingsBtn = this.add.image("400", "375", "settings_btn")
         .setInteractive()
         .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
             this.input.on('pointerdown', () => this.scene.start('settingScene'))
+            this.clickSound.play();
         })
        
 
