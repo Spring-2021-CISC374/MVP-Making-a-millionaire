473ea5acb28df687bbae6708fe79aa7ddde91518
Matt Ward <mattward@udel.edu>
2021-04-07 22:46:20 -0400

    added questions and now they affect score

--- Scene2.js
+++ Scene2.js
@@ -24,26 +24,7 @@ class Scene2 extends Phaser.Scene {
         this.question_frame = this.add.image("0","445","question_frame");
         this.question_frame.setOrigin(0,0);
 
-        this.question_insideA = this.add.image("85","461","question_inside").setInteractive()
-        .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
-            this.input.on('pointerdown', () => this.scene.start('mainMenu'))
-        })
-        this.question_insideA.setOrigin(0,0);
-        this.question_insideB = this.add.image("445","461","question_inside").setInteractive()
-        .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
-            this.input.on('pointerdown', () => this.scene.start('mainMenu'))
-        })
-        this.question_insideB.setOrigin(0,0);
-        this.question_insideC = this.add.image("85","526","question_inside").setInteractive()
-        .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
-            this.input.on('pointerdown', () => this.scene.start('mainMenu'))
-        })
-        this.question_insideC.setOrigin(0,0);
-        this.question_insideD = this.add.image("445","526","question_inside").setInteractive()
-        .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
-            this.input.on('pointerdown', () => this.scene.start('mainMenu'))
-        })
-        this.question_insideD.setOrigin(0,0);
+        
 
         //not sure if this line does anything
         this.cursorKeys = this.input.keyboard.createCursorKeys();
@@ -54,29 +35,126 @@ class Scene2 extends Phaser.Scene {
     }
 
     update() {
-        this.displayQuestion("Which of these is a financial term?","Global Variable","Compound Interest","Convergent Evolution","Three Pointer");
+        var question = ["Which of these is a financial term?","b","Global Variable","Compound Interest","Convergent Evolution","Three Pointer"]
+        this.loadQuestion(question);
     }
 
-    displayQuestion(question,aA,aB,aC,aD) {
-        this.add.text(117,372,question, {
-            font: "24px Arial", 
+    loadQuestion(question) {
+        var answer = question[1];
+
+        if (answer == "a") {
+            this.question_insideA = this.add.image("85","461","question_inside").setInteractive()
+            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+                this.input.on('pointerdown', () => this.add.text(200,200,"RIGHT", {
+                    font: "20px Arial", 
+                    fill: "yellow"}))
+                    console.log("Score: ", this.game.registry.get("score"))
+                    this.game.registry.set("score", this.game.registry.get("score")+5)
+                    console.log("Score: ", this.game.registry.get("score"))
+            })
+        } else {
+            this.question_insideA = this.add.image("85","461","question_inside").setInteractive()
+            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+                this.input.on('pointerdown', () => this.add.text(200,200,"WRONG", {
+                    font: "20px Arial", 
+                    fill: "yellow"}))
+                    console.log("Score: ", this.game.registry.get("score"))
+                    this.game.registry.set("score", this.game.registry.get("score")-5)
+                    console.log("Score: ", this.game.registry.get("score"))
+            })
+        }
+        this.question_insideA.setOrigin(0,0);
+
+        if (answer == "b") {
+            this.question_insideB = this.add.image("445","461","question_inside").setInteractive()
+            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+                this.input.on('pointerdown', () => this.add.text(200,200,"RIGHT", {
+                    font: "20px Arial", 
+                    fill: "yellow"}))
+                    console.log("Score: ", this.game.registry.get("score"))
+                    this.game.registry.set("score", this.game.registry.get("score")+5)
+                    console.log("Score: ", this.game.registry.get("score"))
+                    
+            })
+        } else {
+                this.question_insideB = this.add.image("445","461","question_inside").setInteractive()
+            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+                this.input.on('pointerdown', () => this.add.text(200,200,"WRONG", {
+                    font: "20px Arial", 
+                    fill: "yellow"}))
+                    console.log("Score: ", this.game.registry.get("score"))
+                    this.game.registry.set("score", this.game.registry.get("score")-5)
+                    console.log("Score: ", this.game.registry.get("score"))
+            })
+        }
+        this.question_insideB.setOrigin(0,0);
+
+        if (answer == "c") {
+            this.question_insideC = this.add.image("85","526","question_inside").setInteractive()
+            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+                this.input.on('pointerdown', () => this.add.text(200,200,"RIGHT", {
+                    font: "20px Arial", 
+                    fill: "yellow"}))
+                    console.log("Score: ", this.game.registry.get("score"))
+                    this.game.registry.set("score", this.game.registry.get("score")+5)
+                    console.log("Score: ", this.game.registry.get("score"))
+            })
+        } else {
+                this.question_insideC = this.add.image("85","526","question_inside").setInteractive()
+            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+                this.input.on('pointerdown', () => this.add.text(200,200,"WRONG", {
+                    font: "20px Arial", 
+                    fill: "yellow"}))
+                    console.log("Score: ", this.game.registry.get("score"))
+                    this.game.registry.set("score", this.game.registry.get("score")-5)
+                    console.log("Score: ", this.game.registry.get("score"))
+            })
+        }
+        this.question_insideC.setOrigin(0,0);
+
+        if (answer == "d") {
+            this.question_insideD = this.add.image("445","526","question_inside").setInteractive()
+            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+                this.input.on('pointerdown', () => this.add.text(200,200,"RIGHT", {
+                    font: "20px Arial", 
+                    fill: "yellow"}))
+                    console.log("Score: ", this.game.registry.get("score"))
+                    this.game.registry.set("score", this.game.registry.get("score")+5)
+                    console.log("Score: ", this.game.registry.get("score"))
+            })
+        } else {
+                this.question_insideD = this.add.image("445","526","question_inside").setInteractive()
+            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+                this.input.on('pointerdown', () => this.add.text(200,200,"WRONG", {
+                    font: "20px Arial", 
+                    fill: "yellow"}))
+                    console.log("Score: ", this.game.registry.get("score"))
+                    this.game.registry.set("score", this.game.registry.get("score")-5)
+                    console.log("Score: ", this.game.registry.get("score"))
+            })
+        }
+        this.question_insideD.setOrigin(0,0);
+
+        this.add.text(117,372,question[0], {
+            font: "25px Helvetica", 
             fill: "yellow"
         });
-        this.add.text(85,468,aA, {
+        this.add.text(85,468,question[2], {
             font: "20px Arial", 
             fill: "yellow"
         });
-        this.add.text(445,468,aB, {
+        this.add.text(445,468,question[3], {
             font: "20px Arial", 
             fill: "yellow"
         });
-        this.add.text(85,533,aC, {
+        this.add.text(85,533,question[4], {
             font: "20px Arial", 
             fill: "yellow"
         });
-        this.add.text(445,533,aD, {
+        this.add.text(445,533,question[5], {
             font: "20px Arial", 
             fill: "yellow"
         });
+
     }
 }
